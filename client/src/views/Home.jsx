56e4fcb70193300b533dc2f321145a6d32b9fce5
John Doe <95458294+Danbowman1@users.noreply.github.com>
2022-06-03 17:13:02 -0400

    completed home page

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -10,12 +10,12 @@ const Home = () => {
             <NavBar />
 
             <div className={classes.container}>
-                <Card className='home'>
+                <Card className={classes.homeCard}>
                     <h2>SO, YOU WANT TO TRAVEL TO</h2>
                     <h1>SPACE</h1>
                     <p>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
                 </Card>
-                <div className='homeExplore'>
+                <div className={classes.homeExplore}>
                     <p>EXPLORE</p>
                 </div>
             </div>
